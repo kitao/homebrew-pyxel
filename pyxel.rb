04bd243e4cb00f0eca3dff3a46032febaa463ff0
Takashi Kitao <takashi.kitao@gmail.com>
2021-12-06 11:35:03 +0900

    Updated the sha256

--- pyxel.rb
+++ pyxel.rb
@@ -3,7 +3,7 @@ class Pyxel < Formula
   homepage "https://github.com/kitao/pyxel"
   url "https://github.com/kitao/pyxel/raw/develop/dist/pyxel-1.5.0-macos.zip"
   version "1.5.0pre"
-  sha256 ""
+  sha256 "ef3c3a848fb4042e9d8f148e38f431ef2767b3b04acadd69c0a4a20270e01d33"
 
   def install
     bin.install "pyxel-1.5.0-macos/pyxel"
