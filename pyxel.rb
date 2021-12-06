fb8638a9e0546266a933050dfb7ecc6f4a7aeb3c
Takashi Kitao <takashi.kitao@gmail.com>
2021-12-06 13:11:04 +0900

    Added the license

--- pyxel.rb
+++ pyxel.rb
@@ -4,11 +4,13 @@ class Pyxel < Formula
   url "https://github.com/kitao/pyxel/raw/develop/dist/pyxel-1.5.0-macos.zip"
   version "1.5.0pre"
   sha256 "ef3c3a848fb4042e9d8f148e38f431ef2767b3b04acadd69c0a4a20270e01d33"
+  license "MIT"
 
   def install
-    bin.install "pyxel-1.5.0-macos/pyxel"
+    bin.install "pyxel"
   end
 
   test do
+    #
   end
 end
