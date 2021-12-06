59f0e84c0f3f968318c05cd5f3a0e067d0e8cd1f
Takashi Kitao <takashi.kitao@gmail.com>
2021-12-06 11:26:57 +0900

    Restored the version

--- pyxel.rb
+++ pyxel.rb
@@ -4,7 +4,7 @@ class Pyxel < formula
   desc "A retro game engine for Python"
   homepage "https://github.com/kitao/pyxel"
   url "https://github.com/kitao/pyxel/raw/develop/dist/pyxel-1.5.0-macos.zip"
-  version "1.5.0"
+  version "1.5.0pre"
   sha256 ""
 
   def install
