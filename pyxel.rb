f49cfdaf2c2f7d91d8a56f13fa354835bcf26256
Takashi Kitao <takashi.kitao@gmail.com>
2021-12-11 12:05:07 +0900

    Added sha256

--- pyxel.rb
+++ pyxel.rb
@@ -5,12 +5,12 @@ class Pyxel < Formula
 
   on_macos do
     url "https://github.com/kitao/pyxel/releases/download/v1.5.0/pyxel-1.5.0-macos.zip"
-    sha256 ""
+    sha256 "022c49c0ba46112bc636f8d9c14a39ed11c7795a5c4f60504ff2b9aed6eb91f4"
   end
 
   on_linux do
     url "https://github.com/kitao/pyxel/releases/download/v1.5.0/pyxel-1.5.0-linux.zip"
-    sha256 ""
+    sha256 "6559f58808fe2e117eb5b760ac7e71962f26df754f6858da6a1fe4d2495b4ae6"
   end
 
   def install
