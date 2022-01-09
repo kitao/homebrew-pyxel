18d5c46c178bddfaf5e729d671824b224683a30c
Takashi Kitao <takashi.kitao@gmail.com>
2022-01-09 22:06:10 +0900

    Updated the links

--- pyxel.rb
+++ pyxel.rb
@@ -4,13 +4,13 @@ class Pyxel < Formula
   license "MIT"
 
   on_macos do
-    url "https://github.com/kitao/pyxel/releases/download/v1.5.8/pyxel-1.5.8-macos.zip"
-    sha256 "604d288029ffd10c737ec68016e27a62a3bcbdec98dd88d3a2228e24222cb690"
+    url "https://github.com/kitao/pyxel/releases/download/v1.6.0/pyxel-1.6.0-macos.zip"
+    sha256 "8c7e61ebe0894c2e0bd2ba32a6681438ca4f5aab666aadc60de28128692dcfc7"
   end
 
   on_linux do
-    url "https://github.com/kitao/pyxel/releases/download/v1.5.8/pyxel-1.5.8-linux.zip"
-    sha256 "816b3461321d9353923513bbc34b23edc7a445f8bfab1dcd63b6666eebc1b94d"
+    url "https://github.com/kitao/pyxel/releases/download/v1.6.0/pyxel-1.6.0-linux.zip"
+    sha256 "961e3481d4b1a99614ba27c0f8a2095c4e0bd0f57c4c97476e44d7131a8aafd1"
   end
 
   def install
