3b7d0aad8b3a3005b9cbe4a6f88c74e979b19fda
GitHub Action <action@github.com>
2022-01-21 13:52:08 +0000

    Updated the links

--- pyxel.rb
+++ pyxel.rb
@@ -4,13 +4,13 @@ class Pyxel < Formula
   license "MIT"
 
   on_macos do
-    url "https://github.com/kitao/pyxel/releases/download/v1.6.5/pyxel-1.6.5-macos.zip"
-    sha256 "09f7257e2853284431c432ff672e3349ba51d11a163b4e65711a5b7227e6d15a"
+    url "https://github.com/kitao/pyxel/releases/download/v1.6.6/pyxel-1.6.6-macos.zip"
+    sha256 "29a4cdaf6aa0847865a05d96022b8225b1142a561139d126be0992133e492bea"
   end
 
   on_linux do
-    url "https://github.com/kitao/pyxel/releases/download/v1.6.5/pyxel-1.6.5-linux.zip"
-    sha256 "962320a4be4dffd73739845362536b777efa25a5bf27f4b5341b00f9a666be18"
+    url "https://github.com/kitao/pyxel/releases/download/v1.6.6/pyxel-1.6.6-linux.zip"
+    sha256 "625bc7592f8bc3fc20b2fa6c020a7f410fde5c9ea761783af70065ed8d355882"
   end
 
   def install
