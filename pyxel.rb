a395194410ec43978049708604c0e63238e8c7ab
GitHub Action <action@github.com>
2022-07-20 14:31:52 +0000

    Updated the links

--- pyxel.rb
+++ pyxel.rb
@@ -4,13 +4,13 @@ class Pyxel < Formula
   license "MIT"
 
   on_macos do
-    url "https://github.com/kitao/pyxel/releases/download/v1.7.1/pyxel-1.7.1-macos.zip"
-    sha256 "b5ce42374310180f97f71eba4798ae80796164406bc868392ee1c40016ed6c54"
+    url "https://github.com/kitao/pyxel/releases/download/v1.7.2/pyxel-1.7.2-macos.zip"
+    sha256 "e2f6f58a8d0d9789ffaec5863ceffc2d98e7e52c3a0a95290c53aabcc5c000f7"
   end
 
   on_linux do
-    url "https://github.com/kitao/pyxel/releases/download/v1.7.1/pyxel-1.7.1-linux.zip"
-    sha256 "d560ff8d7a480eea1b489f2cbfc655615491a7a0873cafb78fadf35df51721e2"
+    url "https://github.com/kitao/pyxel/releases/download/v1.7.2/pyxel-1.7.2-linux.zip"
+    sha256 "d63f6b6c87928d9627938054de682c94165f89c8c225f913946f0926bdc22fba"
   end
 
   def install
